7760bb7efac848ef47649318df86b69396ecef06
unknown <jjatuah@gmail.com>
2023-01-18 20:18:11 +0100

    completed the navbar

--- netflix-frontend/src/components/navbar/Navbar.jsx
+++ netflix-frontend/src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import { IoIosNotifications } from "react-icons/io"; 
 import { TiArrowSortedDown } from "react-icons/ti";
@@ -6,8 +7,14 @@ import "./Navbar.scss";
 
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset <= 80 ? false : true); 
+    console.log(isScrolled);
+    return () => (window.onscroll = null)
+  }
   return ( 
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled " : "navbar"}>
       <div className="navContainer">
         <div className="leftNav">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="netflix logo" />
@@ -19,12 +26,18 @@ const Navbar = () => {
         </div>
 
         <div className="rightNav">
-          <BiSearch/>
+          <BiSearch className="icon"/>
           <span>KID</span>
-          <IoIosNotifications/>
+          <IoIosNotifications className="icon"/>
           <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="profile picture" />
-          <BiDownArrow/>
-          {/* <TiArrowSortedDown /> */}
+          <div className="profile">
+            <BiDownArrow className="icon"/>
+            {/* <TiArrowSortedDown /> */}
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
 
         </div>
       </div>
