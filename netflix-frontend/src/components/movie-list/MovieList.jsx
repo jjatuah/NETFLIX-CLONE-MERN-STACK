fada751b852035ba092ac8695d48b0b123b928db
unknown <jjatuah@gmail.com>
2023-01-19 16:59:39 +0100

    started working on the ListItem component

--- netflix-frontend/src/components/movie-list/MovieList.jsx
+++ netflix-frontend/src/components/movie-list/MovieList.jsx
@@ -1,10 +1,21 @@
 import "./MovieList.scss"
+import { MdArrowForwardIos } from "react-icons/md";
+import { MdArrowBackIos } from "react-icons/md";
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
 
 
 const MovieList = () => {
   return ( 
     <div className="movieList">
-      
+      <span className="movieListTitle">Continue to watch</span>
+      <div className="wrapper">
+      <MdArrowForwardIos />
+      <div className="container">
+        
+      </div>
+      <MdArrowBackIos />
+      </div>
     </div>
    );
 }
--- netflix-frontend/src/pages/home/Home.jsx
+++ netflix-frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import "./Home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
+import MovieList from "../../components/movie-list/MovieList";
 
 const Home = () => {
   return ( 
     <div className="home">
       <Navbar />
       <Featured type={"movie"}/>
+      <MovieList />
     </div>
    );
 }
