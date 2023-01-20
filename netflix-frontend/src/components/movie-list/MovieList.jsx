fc7a803fa174d9165a9f07ec34415867bca06fe6
unknown <jjatuah@gmail.com>
2023-01-20 16:02:45 +0100

    completed ListItem component

--- netflix-frontend/src/components/movie-list/MovieList.jsx
+++ netflix-frontend/src/components/movie-list/MovieList.jsx
@@ -40,16 +40,16 @@ const MovieList = () => {
       <div className="wrapper">
         <MdArrowBackIos className="sliderArrow left" onClick={() => handleClick("left")} style={{display: !isMoved && "none"}} />
         <div className="container" ref={movieListRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />        
+          <ListItem index = {0} />
+          <ListItem index = {1} />
+          <ListItem index = {2} />
+          <ListItem index = {3} />
+          <ListItem index = {4} />
+          <ListItem index = {5} />
+          <ListItem index = {6} />
+          <ListItem index = {7} />
+          <ListItem index = {8} />
+          <ListItem index = {9} />        
         </div>
         <MdArrowForwardIos className="sliderArrow right" onClick={() => handleClick("right")} />
       </div>
