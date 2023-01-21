a5cbe3189b7e5b432d18f30dde9d54ea92ba21cf
unknown <jjatuah@gmail.com>
2023-01-21 01:22:57 +0100

    completed the jsx code for the login page

--- netflix-frontend/src/pages/login/Login.jsx
+++ netflix-frontend/src/pages/login/Login.jsx
@@ -1,10 +1,34 @@
 import "./Login.scss";
+import NetflixLogo from "../../assets/NetflixLogo.png"
 
 
 const Login = () => {
   return ( 
     <div className="login">
-      
+      <div className="top">
+        <div className="topWrapper">
+          <img
+            className="logo"
+            src={NetflixLogo}
+            alt="Netflix Logo"
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or phone number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>
+            New to Netflix? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>.
+          </small>
+        </form>
+      </div>
     </div>
    );
 }
