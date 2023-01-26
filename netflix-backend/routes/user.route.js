807a25e4efe29183605ed1c3b98a51b5e3136292
unknown <jjatuah@gmail.com>
2023-01-27 00:57:05 +0100

    completed delete request for userRoute

--- netflix-backend/routes/user.route.js
+++ netflix-backend/routes/user.route.js
@@ -33,4 +33,19 @@ userRoute.put("/:id", verify, async (req, res) => {
   }
 })
 
+
+//Delete
+userRoute.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await UserModel.findByIdAndDelete(req.params.id)
+      res.status(200).json("Account deleted successfully")
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json("You can only delete your account")
+  }
+})
+
 module.exports = userRoute
