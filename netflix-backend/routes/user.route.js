44b60a2a8bdc7906845da20b7ac61917be2db639
unknown <jjatuah@gmail.com>
2023-01-27 00:38:16 +0100

    completed the put request for the userRoute

--- netflix-backend/routes/user.route.js
+++ netflix-backend/routes/user.route.js
@@ -2,10 +2,35 @@ const express = require('express');
 const UserModel = require("../models/user.model");
 const CryptoJS = require("crypto-js");
 const jwt = require('jsonwebtoken');
+const verify = require("../verifyToken");
 require('dotenv').config();
 
 
 const userRoute = express.Router();
 
+//Update
+userRoute.put("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString();
+    }
+
+    try {
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        req.params.id,
+        {$set: req.body}, {new:true}
+      );
+
+      res.status(200).json(updatedUser)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json("You can only update your account")
+  }
+})
 
 module.exports = userRoute
