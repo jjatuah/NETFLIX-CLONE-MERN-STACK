2efaa560534a9a209eadee269f5f7c062ea509a8
unknown <jjatuah@gmail.com>
2023-01-26 11:47:56 +0100

    completed users schema

---
+++ netflix-backend/models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const UserId = Schema.ObjectId; 
+
+const UserSchema = new Schema (
+    {
+      id: UserId,
+
+      username: {
+        type: String,
+        required: true,
+        unique: true
+      },
+
+      email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+
+      password: {
+        type: String,
+        required: true
+      },
+
+      profilePic: {
+        type: String,
+        default: "",
+      },
+
+      admin: {
+        type: Boolean,
+        default: false
+      }
+  },
+  { timestamps: true }
+);
+
+
+module.export = mongoose.model("Users", UserSchema)
