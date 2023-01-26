dfe8a9a48c15269db1c594722a455abf63a75a05
unknown <jjatuah@gmail.com>
2023-01-26 21:07:27 +0100

    completed signin auth route

--- netflix-backend/routes/authentication.route.js
+++ netflix-backend/routes/authentication.route.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const UserModel = require("../models/user.model");
 const CryptoJS = require("crypto-js");
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
 
 
 const authRoute = express.Router();
@@ -28,7 +27,34 @@ authRoute.post("/signup", async (req, res) => {
   }
 })
 
+//Sign-in
+authRoute.post("/signin", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({email: req.body.email});
 
+    !user && res.status(401).json("Wrong email provided");
+
+    const originalPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password  && res.status(401).json("Wrong password provided")
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin
+      }, process.env.SECRET_KEY, {expiresIn: "1h"}
+    );
+
+    console.log(accessToken);
+
+    const {password, ...info} = user._doc
+
+    res.status(200).json({info, accessToken})
+    
+  } catch (error) {
+    res.status(500).json(error);
+  } 
+})
 
 
 module.exports = authRoute;
