08568da57fac34aab20219727ceeeaf9468256f2
unknown <jjatuah@gmail.com>
2023-01-27 12:38:22 +0100

    completed put method for movielist route

--- netflix-backend/routes/movieList.route.js
+++ netflix-backend/routes/movieList.route.js
@@ -22,5 +22,27 @@ movieListRoute.post("/", verify, async (req, res) => {
 })
 
 
+//Update List
+movieListRoute.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovieList = await MovieListModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body
+        },
+        {new: true}
+        )
+      res.status(200).json(updatedMovieList);
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json("You are not allowed")
+  }
+})
+
+
+
 
 module.exports = movieListRoute;
