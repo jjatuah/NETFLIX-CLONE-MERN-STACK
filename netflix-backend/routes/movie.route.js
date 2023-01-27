6e91441ab2a3cf76c03490bf6383bcf013cd69a6
unknown <jjatuah@gmail.com>
2023-01-27 11:13:35 +0100

    completed get randommovie for movie route

--- netflix-backend/routes/movie.route.js
+++ netflix-backend/routes/movie.route.js
@@ -48,12 +48,47 @@ movieRoute.delete("/:id", verify, async (req, res) => {
   if (req.user.isAdmin) {
     try {
       await MovieModel.findByIdAndDelete(req.params.id);
-      res.status(200).json("The moviehas been deleted");
+      res.status(200).json("The movie has been deleted");
     } catch (error) {
       res.status(500).json(error)
     }
   } else {
     res.status(403).json("You are not allowed")
+  }
+})
+
+
+//Get Movie by id
+movieRoute.get("/:id", verify, async (req, res) => {
+  try {
+    const movie = await MovieModel.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({message:"movie not found", error})
+  }
+})
+
+
+//Get random movie 
+movieRoute.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await MovieModel.aggregate([
+        {$match: {isSeries: true}},
+        {$sample: {size: 1}},
+      ]);
+    } else {
+      movie = await MovieModel.aggregate([
+        {$match: {isSeries: false}},
+        {$sample: {size: 1}},
+      ]);
+    }
+    
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({message:"movie not found", error})
   }
 })
 
