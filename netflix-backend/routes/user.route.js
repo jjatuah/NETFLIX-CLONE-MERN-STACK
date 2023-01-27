a3b0863f304cdc6138ef6a8069a39b6d0aec53c5
unknown <jjatuah@gmail.com>
2023-01-27 02:13:43 +0100

    completed get method for user stats on userRoute

--- netflix-backend/routes/user.route.js
+++ netflix-backend/routes/user.route.js
@@ -78,4 +78,36 @@ userRoute.get("/", verify, async (req, res) => {
 })
 
 
+//get user stats (for admin only)
+userRoute.get("/stats", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const today = new Date();
+    const lastYear = today.setFullYear(today.setFullYear() - 1);
+
+    const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+   
+    try {
+      const data = await UserModel.aggregate([
+        {
+          $project: {
+            month: {$month: "$createdAt"},
+          }, 
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1},
+          },
+        },
+      ])
+      res.status(200).json(data)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json("You're not allowed to see all users")
+  }
+})
+
+
 module.exports = userRoute
