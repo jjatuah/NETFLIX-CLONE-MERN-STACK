4e45993f37a5f0d885f73b1ec04793b1b54e3fd4
unknown <jjatuah@gmail.com>
2023-01-27 01:06:15 +0100

    completed get request by id for userRoute

--- netflix-backend/routes/user.route.js
+++ netflix-backend/routes/user.route.js
@@ -48,4 +48,17 @@ userRoute.delete("/:id", verify, async (req, res) => {
   }
 })
 
+
+//get user by id
+userRoute.get("/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id)
+    const {password, ...info} = user._doc
+    res.status(200).json(info)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+
 module.exports = userRoute
