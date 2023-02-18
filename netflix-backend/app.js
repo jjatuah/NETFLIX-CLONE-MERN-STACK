3f8b48e5524e48ed1e812102460d3e5b4efb2ebf
unknown <jjatuah@gmail.com>
2023-02-18 17:58:06 +0100

    fixed cors bug

--- netflix-backend/app.js
+++ netflix-backend/app.js
@@ -5,10 +5,19 @@ const authRoute = require("./routes/authentication.route");
 const userRoute = require("./routes/user.route");
 const movieRoute = require("./routes/movie.route");
 const movieListRoute = require("./routes/movieList.route");
+const cors = require("cors");
 
 const app = express();
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000 
+
+const corsOrigin ={
+  origin:'http://localhost:3000', //or whatever port your frontend is using
+  credentials:true,            
+  optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
+
 
 // Connecting to mongo DB
 connectToMongoDB()
@@ -16,6 +25,20 @@ connectToMongoDB()
 //Connecting all middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}));
+
+
+// app.get("/", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader("Access-Control-Allow-Headers", "content-type");
+//   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );  
+//  })
+
+app.get("/bb", (req, res) => {
+ return res.json("Home Route") 
+})
+
 
 //Connecting all routes
 app.use("/api/auth", authRoute)
--- netflix-frontend/src/pages/home/Home.jsx
+++ netflix-frontend/src/pages/home/Home.jsx
@@ -2,8 +2,31 @@ import "./Home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import MovieList from "../../components/movie-list/MovieList";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Home = ({type}) => {
+
+  const [list, setList] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getLists = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/movielist${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`, {
+          headers: {
+            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDJjNTBjOTA2ZDg1MDA4ZWNmOTY4YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NjczODg0MSwiZXhwIjoxNjc2NzQyNDQxfQ.Bho8o8s41XYDQQwrHxorXWmcZ_MhNee40c4L2LSELO8"
+          }
+        });  
+
+        console.log(response);
+      } catch (error) { 
+        // console.log(error);
+      }
+    }
+    getLists()
+  }, [type, genre])
   return ( 
     <div className="home">
       <Navbar />
