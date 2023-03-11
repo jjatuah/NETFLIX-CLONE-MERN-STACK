118b49cbf6c162893efa162997178249101fa0ff
unknown <jjatuah@gmail.com>
2023-03-11 22:49:33 +0100

    completed apiCalls.js

--- netflix-admin/src/context/authContext/apiCalls.js
+++ netflix-admin/src/context/authContext/apiCalls.js
@@ -1,2 +1,13 @@
 import axios from "axios";
+import { loginFailure, loginSuccess, loginStart } from "./AuthActions";
 
+export const login = async (user, dispatch) => {
+  dispatch(loginStart());
+
+  try {
+    const response = await axios.post("http://localhost:8000/api/auth/signin", user);
+    dispatch(loginSuccess(response.data))
+  } catch (error) {
+    dispatch(loginFailure());
+  }
+}
