b47b981bc75a120d27798ea04081341db780718d
unknown <jjatuah@gmail.com>
2023-03-12 18:46:23 +0100

    added apiCall for delete movie actions

--- netflix-admin/src/context/movieContext/apiCalls.js
+++ netflix-admin/src/context/movieContext/apiCalls.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getMoviesFailure, getMoviesSuccess, getMoviesStart } from "./MovieActions";
+import { getMoviesFailure, getMoviesSuccess, getMoviesStart, deleteMovieStart, deleteMovieSuccess, deleteMovieFailure} from "./MovieActions";
 
 
 export const getMovies = async (dispatch) => {
@@ -14,5 +14,22 @@ export const getMovies = async (dispatch) => {
     dispatch(getMoviesSuccess(response.data))
   } catch (error) {
     dispatch(getMoviesFailure())
+  }
+}
+
+//Delete
+
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+
+  try {
+    await axios.delete("http://localhost:8000/api/movie/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+      },
+    });
+    dispatch(deleteMovieSuccess(id))
+  } catch (error) {
+    dispatch(deleteMovieFailure())
   }
 }
