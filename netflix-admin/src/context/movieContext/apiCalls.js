b3d084b5f077ae661589e3c713708827ddcee89f
unknown <jjatuah@gmail.com>
2023-03-12 10:24:20 +0100

    completed apiCalls.js for movies

---
+++ netflix-admin/src/context/movieContext/apiCalls.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getMoviesFailure, getMoviesSuccess, getMoviesStart } from "./MovieActions";
+
+
+export const getMovies = async (dispatch) => {
+  dispatch(getMoviesStart());
+
+  try {
+    const response = await axios.get("http://localhost:8000/api/movie", {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+      },
+    });
+    dispatch(getMoviesSuccess(response.data))
+  } catch (error) {
+    dispatch(getMoviesFailure())
+  }
+}
